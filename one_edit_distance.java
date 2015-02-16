9a253202c875dfa3c0b117c48db4854d5b0b3d5b
adamxrc <xinruichao@qq.com>
2015-02-16 12:57:56 +0800

    Create one_edit_distance.java

---
+++ one_edit_distance.java
@@ -0,0 +1,9 @@
+public class Solution {
+  public boolean isOneEditDistance(String s, String t) {
+    return check(s, t, 0, 0, 0);
+  }
+  
+  private boolean check(String s, String t, int i, int j, int distance) {
+    
+  }
+}
