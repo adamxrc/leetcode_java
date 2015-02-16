8b48c24226dec0bf0dd4b0048cbe5be2c60f1734
adamxrc <xinruichao@qq.com>
2015-02-16 13:01:27 +0800

    Update one_edit_distance.java

--- one_edit_distance.java
+++ one_edit_distance.java
@@ -4,6 +4,19 @@ public class Solution {
   }
   
   private boolean check(String s, String t, int i, int j, int distance) {
+    while(i >= 0 && i < s.length() && j >= 0 && j < t.length()) {
+      if(s.charAt(i) != t.charAt(j)) {
+        distance++;
+        if(distance > 1)
+          return false;
+        return check(s, t, i+1, j, distance) || check(s, t, i, j+1, distance) || check(s, t, i+1, j+1, distance);
+      } else
+        return check(s, t, i+1, j+1, distance);
+    }
     
+    if(distance == 1)
+      return i == s.length() && j == t.length();
+    else
+      return Math.abs(s.length()-t.length()) == 1;
   }
 }
