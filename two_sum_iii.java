d9a15ce6917f142e762455eef8912356084a36c6
adamxrc <xinruichao@qq.com>
2015-02-16 11:17:49 +0800

    Update two_sum_iii.java

--- two_sum_iii.java
+++ two_sum_iii.java
@@ -1,2 +1,22 @@
 public class TwoSum {
+  Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+  
+  public void add(int value) {
+    if(map.containsKey(value)) {
+      map.put(value, map.get(value)+1);
+    } else {
+      map.put(value, 1);
+    }
+  }
+  
+  public void find(int value) {
+    for(int key : map.keySet()) {
+      int another = value - key;
+      if(another == key && map.get(key) > 1)
+        return true;
+      else if(another != key && map.containsKey(another))
+        return true;
+    }
+    return false;
+  }
 }
