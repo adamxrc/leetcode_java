268cd705bcf2cb4c47081ab4d25c6a2f162f0d89
adamxrc <xinruichao@qq.com>
2015-02-16 13:07:55 +0800

    Update read_n_characters_given_read4.java

--- read_n_characters_given_read4.java
+++ read_n_characters_given_read4.java
@@ -6,7 +6,8 @@ public class Solution extends Reader4 {
     char[] temp = new char[4]; 
     while(n>0) { 
       int x = read4(temp); 
-      int j = 0; int k = x; 
+      int j = 0; 
+      int k = x; 
       while(n>0 && x>0) { 
         buf[i++] = temp[j++]; 
         --x; 
