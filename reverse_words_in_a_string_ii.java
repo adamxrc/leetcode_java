bf5f48a5f5b8749b8bb40472173918f67b8025e3
adamxrc <xinruichao@qq.com>
2015-02-16 10:24:26 +0800

    Update reverse_words_in_a_string_ii.java

--- reverse_words_in_a_string_ii.java
+++ reverse_words_in_a_string_ii.java
@@ -1,3 +1,34 @@
 public class Solution {
-
+  public String reverse(String input) {
+    char[] chars = input.toCharArray();
+    int len = chars.length;
+    reverse(chars, 0, len-1);
+    int wordStart = -1;
+    int wordEnd= -1;
+    for(int i = 0; i < len; i++) {
+      if(chars[i] == ' ' || i == len-1) {
+        if(wordStart >= 0 && wordEnd >= 0) {
+          reverse(chars, wordStart, wordEnd);
+          wordStart = -1;
+          wordEnd = -1;
+        }
+      } else {
+        if(wordStart == -1)
+          wordStart = i;
+        wordEnd = i;
+      }
+    }
+    
+    return new String(chars);
+  }
+  
+  private void reverse(char[] chars, int start, int end) {
+    while(start < end) {
+      char temp = chars[start];
+      chars[start] = chars[end];
+      chars[end] = temp;
+      start++;
+      end--;
+    }
+  }
 }
