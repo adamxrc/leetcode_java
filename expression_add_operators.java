ae725c01f293d6168a8c885f91b8d146f2655a9c
adamxrc <xinruichao@qq.com>
2016-07-18 22:54:43 +0800

    Update expression_add_operators.java

--- expression_add_operators.java
+++ expression_add_operators.java
@@ -1,2 +1,34 @@
 public class Solution {
+  
+  List<String> res = new ArrayList<String>();
+  
+  public List<String> addOperators(String num, int target) {
+    helper(num, target, "", 0, 0);
+    return res;
+  }
+  
+  private void helper(String num, int target, String tmp, int currRes, int prevNum) {
+    if(currRes == target && num.length() == 0) {
+      String exp = new String(tmp);
+      res.add(exp);
+      return;
+    }
+    
+    for(int i = 1; i <= num.length(); i++) {
+      String currStr = num.substring(0, i);
+      if(currStr.length() > 1 && currStr.charAt(0) == '0') {
+        return;
+      }
+      long currNum = Long.parseLong(currStr);
+      String next = num.substring(i);
+      if(tmp.length() != 0) {
+        helper(next, target, tmp+"*"+currNum, (currRes-prevNum)+prevNum*currNum, prevNum*currNum);
+        helper(next, target, tmp+"+"+currNum, currRes+currNum, currNum);
+        helper(next, target, tmp+"-"+currNum, currRes-currNum, -currNum);
+      } else {
+        helper(next, target, currStr, currNum, currNum);
+      }
+    }
+  }
+  
 }
