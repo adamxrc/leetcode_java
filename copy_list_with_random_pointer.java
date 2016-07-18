7f830b4a1bf0dfb765391d70d07565a61c854fb7
adamxrc <xinruichao@qq.com>
2016-07-19 06:39:59 +0800

    Update copy_list_with_random_pointer.java

--- copy_list_with_random_pointer.java
+++ copy_list_with_random_pointer.java
@@ -6,5 +6,31 @@
  *     RandomListNode(int x) { this.label = x; }
  * };
  */
- public class Solution {
- }
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        RandomListNode fake = new RandomListNode(0);
+        RandomListNode src = head;
+        RandomListNode target = fake;
+        
+        while(src != null) {
+            RandomListNode node = new RandomListNode(src.label);
+            map.put(src, node);
+            target.next = node;
+            src = src.next;
+            target = target.next;
+        }
+        
+        src = head;
+        while(src != null) {
+            if(src.random != null) {
+                RandomListNode from = map.get(src);
+                RandomListNode to = map.get(src.random);
+                from.random = to;
+            }
+            src = src.next;
+        }
+        
+        return fake.next;
+    }
+}
