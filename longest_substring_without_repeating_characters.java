a2735098d21e755321ba5e6e3570e97d31954334
adamxrc <xinruichao@qq.com>
2016-07-24 17:23:16 +0800

    Update longest_substring_without_repeating_characters.java

--- longest_substring_without_repeating_characters.java
+++ longest_substring_without_repeating_characters.java
@@ -1,1 +1,29 @@
-
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int slow = 0;
+        int fast = 0;
+        int max = 0;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        while(fast < s.length()) {
+            char ch = s.charAt(fast);
+            
+            if(slow == fast) {
+                map.put(ch, fast);
+                fast++;
+                continue;
+            }
+            
+            if(map.containsKey(ch)) {
+                max = Math.max(max, fast-slow);
+                int newSlow = map.get(ch);
+                for(int i = slow; i <= newSlow; i++)
+                    map.remove(s.charAt(i));
+                slow = newSlow + 1;
+            } else {
+                map.put(ch, fast);
+                fast++;
+            }
+        }
+        return Math.max(max, fast-slow);
+    }
+}
