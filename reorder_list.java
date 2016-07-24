5edd321450319e63eb08c322f104a3d6108f23f8
adamxrc <xinruichao@qq.com>
2016-07-24 22:31:28 +0800

    Update reorder_list.java

--- reorder_list.java
+++ reorder_list.java
@@ -1,1 +1,27 @@
-
+public class Solution {
+    public void reorderList(ListNode head) {
+        Stack<ListNode> stack = new Stack<ListNode>();
+        ListNode cur = head;
+        while(cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        cur = head;
+        while(cur != null) {
+            ListNode n = stack.pop();
+            if(cur != n) {
+                ListNode temp = cur;
+                cur = cur.next;
+                temp.next = n;
+                n.next = cur;
+                if(cur.next == n) {
+                    cur.next = null;
+                    break;
+                }
+            } else {
+                cur.next = null;
+                break;
+            }
+        }
+    }
+}
