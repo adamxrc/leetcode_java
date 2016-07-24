b2ee3d2e06c46baf4af3dd47e746804e702a4b48
adamxrc <xinruichao@qq.com>
2016-07-24 17:01:09 +0800

    Update range_sum_query_2d.java

--- range_sum_query_2d.java
+++ range_sum_query_2d.java
@@ -1,1 +1,42 @@
-
+public class NumMatrix {
+    private int[][] sum;
+    
+    public NumMatrix(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            sum = new int[][]{};
+            return;
+        }
+        
+        sum = new int[matrix.length][matrix[0].length];
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(i == 0) {
+                    if(j == 0)
+                        sum[i][j] = matrix[0][0];
+                    else
+                        sum[i][j] = sum[i][j-1] + matrix[i][j];
+                } else {
+                    if(j == 0)
+                        sum[i][j] = sum[i-1][j] + matrix[i][j];
+                    else
+                        sum[i][j] = sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1] + matrix[i][j];
+                }
+            }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int left = 0;
+        if(col1 > 0)
+            left = sum[row2][col1-1];
+        
+        int top = 0;
+        if(row1 > 0)
+            top = sum[row1-1][col2];
+            
+        int leftTop = 0;
+        if(row1 > 0 && col1 > 0)
+            leftTop = sum[row1-1][col1-1];
+            
+        return sum[row2][col2] - left - top + leftTop;
+    }
+}
