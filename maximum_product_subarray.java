1ddab0405bfaeb2837701d007fe7f344faccbf26
adamxrc <xinruichao@qq.com>
2016-07-24 18:26:39 +0800

    Update maximum_product_subarray.java

--- maximum_product_subarray.java
+++ maximum_product_subarray.java
@@ -1,1 +1,21 @@
-
+public class Solution {
+    public int maxProduct(int[] A) {
+        int max = Integer.MIN_VALUE;
+        int positive = 1;
+        int negative = 1;
+        for(int a: A) {
+            int n1 = positive*a;
+            int n2 = negative*a;
+            int n3 = a;
+            
+            positive = Math.max(n1, n2);
+            positive = Math.max(positive, n3);
+            
+            negative = Math.min(n1, n2);
+            negative = Math.min(negative, n3);
+            
+            max = Math.max(max, positive);
+        }
+        return max;
+    }
+}
