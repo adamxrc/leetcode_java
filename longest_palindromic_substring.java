4aee9037bf09c8ed24a5af248e10da8c17d60809
adamxrc <xinruichao@qq.com>
2016-07-24 22:58:17 +0800

    Update longest_palindromic_substring.java

--- longest_palindromic_substring.java
+++ longest_palindromic_substring.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+    public String longestPalindrome(String s) {
+        String sub = "";
+        for(int i = 0; i < s.length(); i++) {
+            int j;
+            
+            for(j = 0; i-j>=0 && i+j<s.length(); j++) {
+                if(s.charAt(i-j) == s.charAt(i+j)) {
+                    if(j*2+1 > sub.length())
+                        sub = s.substring(i-j, i+j+1);
+                } else
+                    break;
+            }
+            
+            for(j = 0; i-j>=0 && i+j+1<s.length(); j++) {
+                if(s.charAt(i-j) == s.charAt(i+j+1)) {
+                    if((j+1)*2 > sub.length())
+                        sub = s.substring(i-j, i+j+2);
+                } else
+                    break;
+            }
+        }
+        return sub;
+    }
+}
