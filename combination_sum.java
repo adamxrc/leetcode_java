5d1c4ce9867b47df701a6240ebb1dc166d18522d
adamxrc <xinruichao@qq.com>
2016-08-08 16:39:07 +0800

    Update combination_sum.java

--- combination_sum.java
+++ combination_sum.java
@@ -1,1 +1,31 @@
-
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(candidates == null)
+            return result;
+        List<Integer> path = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        helper(candidates, target, path, 0, result);
+        return result;
+    }
+    
+    private void helper(int[] candidates, int target, List<Integer> path, int index, 
+        List<List<Integer>> result) {
+        if(target == 0) {
+            result.add(new ArrayList<Integer>(path));
+            return;
+        }    
+        
+        int prev = -1;
+        for(int i = index; i < candidates.length; i++) {
+            if(candidates[i] > target)
+                break;
+            if(prev != -1 && prev == candidates[i])
+                continue;
+            path.add(candidates[i]);
+            helper(candidates, target-candidates[i], path, i, result);
+            path.remove(path.size()-1);
+            prev = candidates[i];
+        }
+    }
+}
