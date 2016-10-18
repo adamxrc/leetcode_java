d5acd68be7fd0aa55431d5b9b4f8474799ec7a30
adamxrc <xinruichao@qq.com>
2016-10-18 16:28:24 +0800

    Update group_shifted_string.java

--- group_shifted_string.java
+++ group_shifted_string.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for(String str: strings) {
+            String key = "";
+            for(int i = 1; i < str.length(); i++) {
+                int offset = str.charAt(i) - str.charAt(i-1);
+                key += offset < 0 ? offset+26: offset;
+            }
+            if(!map.containsKey(key))
+                map.put(key, new ArrayList<String>());
+            map.get(key).add(str);
+        }
+        for(List<String> list: map.values())
+            result.add(list);
+        return result;
+    }
+}
