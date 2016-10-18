107c01d8e51a2e565d60c58dd3c1ce929d477b80
adamxrc <xinruichao@qq.com>
2016-10-18 15:20:01 +0800

    Update meeting_rooms.java

--- meeting_rooms.java
+++ meeting_rooms.java
@@ -1,1 +1,27 @@
+/** 
+ * Definition for an interval. 
+ * public class Interval { 
+ *     int start; 
+ *     int end; 
+ *     Interval() { start = 0; end = 0; } 
+ *     Interval(int s, int e) { start = s; end = e; } 
+ * } 
+ */ 
 
+public class Solution {
+    public boolean canAttendMeetings(Interval[] intervals) {
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        for(int i = 0; i < intervals.length; i++) {
+            start[i] = intervals[i].start;
+            end[i] = intervals[i].end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        for(int i = 1; i < intervals.length; i++) {
+            if(end[i-1] > start[i])
+                return false;
+        }
+        return true;
+    }
+}
