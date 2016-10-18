606d53e5ecd6e037670f4fb6920fb7213ab7c8c9
adamxrc <xinruichao@qq.com>
2016-10-18 22:29:17 +0800

    Update design_hit_counter.java

--- design_hit_counter.java
+++ design_hit_counter.java
@@ -1,1 +1,35 @@
+public class Solution {
+    Hit start = new Hit(0);
+    int count = 0;
+    Hit tail = start;
+    
+    public void hit(int timestamp) {
+        if(tail.timestamp == timestamp) {
+            tail.count++;
+        } else {
+            tail.next = new Hit(timestamp);
+            tail = tail.next;
+        }
+        count++;
+    }
+    
+    public int getHits(int timestamp) {
+        while(start.next != null && timestamp-start.next.timestamp >= 300) {
+            count -= start.next.count;
+            start.next = start.next.next;
+        }
+        if(start.next == null)
+            tail = start;
+        return count;
+    }
+}
 
+class Hit {
+    int timestamp;
+    int count;
+    Hit next;
+    Hit(int timestamp) {
+        this.timestamp = timestamp;
+        this.count = 1;
+    }
+}
