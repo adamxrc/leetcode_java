c521834b2affc4518a86a6162cddd246d7237fa0
adamxrc <xinruichao@qq.com>
2016-10-18 22:40:35 +0800

    Update sparse_matrix_multiplication.java

--- sparse_matrix_multiplication.java
+++ sparse_matrix_multiplication.java
@@ -1,1 +1,16 @@
-
+public class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        int[][] C = new int[A.length][B[0].length];
+        for(int i = 0; i < A.length; i++)
+            for(int j = 0; j < A[0].length; j++) {
+                if(A[i][j] == 0)
+                    continue;
+                for(int k = 0; k < B[0].length; k++) {
+                    if(B[j][k] == 0)
+                        continue;
+                    C[i][k] += A[i][j] * B[j][k];
+                }
+            }
+        return C;
+    }
+}
