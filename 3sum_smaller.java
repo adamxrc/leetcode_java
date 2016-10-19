475eef1ad167d3e6cf8ec7e628a64ec22e6a4899
adamxrc <xinruichao@qq.com>
2016-10-19 17:10:06 +0800

    Update 3sum_smaller.java

--- 3sum_smaller.java
+++ 3sum_smaller.java
@@ -1,2 +1,18 @@
 public class Solution {
+  	public int threeSumSmaller(int[] nums, int target) {
+		Arrays.sort(nums);
+		int count = 0;
+		for(int i = 0; i < nums.length-2; i++) {
+			int j = i+1;
+			int k = nums.length-1;
+			while(j < k) {
+				if(nums[i]+nums[j]+nums[k] < target) {
+					count += k - j;
+					j++;
+				} else
+					k--;
+			}
+		}
+		return count;
+	}
 }
