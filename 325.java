bed7b034bc7fc4b93fca6b963620743d73d3afc9
adamxrc <xinruichao@qq.com>
2016-10-19 13:17:34 +0800

    Update 325.java

--- 325.java
+++ 325.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+	public int maxSubArrayLen(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int len = 0;
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			if(sum == k)
+				len = i+1;
+			if(map.containsKey(sum-k))
+				len = Math.max(len, i-map.get(sum-k));
+			if(map.containsKey(sum))
+				map.put(sum, map.get(sum));
+			else
+				map.put(sum, i);
+		}
+		return len;
+	}
+}
