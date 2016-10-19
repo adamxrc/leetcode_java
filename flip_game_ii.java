803abf16f8ff7d5e012967797c4d97f91a37dd2b
adamxrc <xinruichao@qq.com>
2016-10-19 10:20:18 +0800

    Update flip_game_ii.java

--- flip_game_ii.java
+++ flip_game_ii.java
@@ -1,1 +1,20 @@
-
+public class Solution {
+    public boolean firstPlayWinning(String s) {
+        return canWin(s.toCharArray());
+    }
+    
+    private boolean canWin(char[] chs) {
+        for(int i = 0; i < chs.length-1; i++) {
+            if(chs[i] == chs[i+1] && chs[i] == '+') {
+                chs[i] = '-';
+                chs[i+1] = '-';
+                boolean win = !canWin(chs);
+                chs[i] = '+';
+                chs[i+1] = '+';
+                if(win)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
