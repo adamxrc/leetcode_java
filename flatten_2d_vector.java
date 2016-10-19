f6f797da45ec4839924beb8d5fc1a57ccaaf246d
adamxrc <xinruichao@qq.com>
2016-10-20 06:50:44 +0800

    Update flatten_2d_vector.java

--- flatten_2d_vector.java
+++ flatten_2d_vector.java
@@ -1,1 +1,23 @@
-
+public class Vector2D {
+	List<Iterator<Integer>> its;
+	int curr = 0;
+	
+	public Vector2D(List<List<Integer>> vec2d) {
+		its = new ArrayList<Iterator<Integer>>();
+		for(List<Integer> l : vec2d) {
+			if(l.size() > 0)
+				its.add(l.iterator());
+		}
+	}
+	
+	public int next() {
+		Integer res = its.get(curr).next();
+		if(!its.get(curr).hasNext())
+			curr++;
+		return res;
+	}
+	
+	public boolean hasNext() {
+		return curr < its.size() && its.get(curr).hasNext();
+	}
+}
