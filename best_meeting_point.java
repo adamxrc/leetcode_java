dc68d8be6b1bdafe725414993e0e919a1e44a283
adamxrc <xinruichao@qq.com>
2016-10-20 23:02:56 +0800

    Update best_meeting_point.java

--- best_meeting_point.java
+++ best_meeting_point.java
@@ -1,1 +1,25 @@
-
+public class Solution {
+	public int minTotalDistance(int[][] grid) {
+		List<Integer> x = new ArrayList<>();
+		List<Integer> y = new ArrayList<>();
+		
+		for(int i = 0; i < grid.length; i++)
+			for(int j = 0; j < grid[0].length; j++) {
+				if(grid[i][j] == 1) {
+					x.add(i);
+					y.add(j);
+				}
+			}
+		
+		return getMP(x) + getMP(y);
+	}
+	
+	private int getMP(List<Integer> l) {
+		Collections.sort(l);
+		int res = 0;
+		int i = 0, j = l.size()-1;
+		while(i < j)
+			res += l.get(j--) - l.get(i++);
+		return res;
+	}
+}
