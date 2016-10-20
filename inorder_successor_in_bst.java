e418e3fa246fd7a5556d4753d27f8c0d7388d0ca
adamxrc <xinruichao@qq.com>
2016-10-20 11:13:04 +0800

    Update inorder_successor_in_bst.java

--- inorder_successor_in_bst.java
+++ inorder_successor_in_bst.java
@@ -1,1 +1,27 @@
-public 
+public class Solution {
+  	public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		Stack<TreeNode> stk = new Stack<TreeNode>();
+		TreeNode curr = root;
+		while(curr != p) {
+			stk.push(curr);
+			if(curr.val > p.val) {
+				curr = curr.left;
+			} else {
+				curr = curr.right;
+			}
+		}
+		
+		if(curr.right != null) {
+			curr = curr.right;
+			while(curr.left != null) {
+				curr = curr.left;
+			}
+			return curr;
+		} else {
+			while(!stk.isEmpty() && stk.peek().val < curr.val) {
+				stk.pop():
+			}
+			return stk.isEmpty() ? null : stk.pop();
+		}
+	}
+}
