52d669f8092972943de2a123058a84867f3d9b76
adamxrc <xinruichao@qq.com>
2016-10-20 16:46:58 +0800

    Update design_phone_directory.java

--- design_phone_directory.java
+++ design_phone_directory.java
@@ -1,1 +1,32 @@
-
+public class PhoneDirectory {
+	int max;
+	Set<Integer> set;
+	LinkedList<Integer> queue;
+	
+	public PhoneDirectory(int maxNumber) {
+		set = new HashSet<Integer>();
+		queue = new LinkedList<Integer>();
+		for(int i = 0; i < maxNumber; i++)
+			queue.offer(i);
+		max = maxNumber - 1;
+	}
+	
+	public int get() {
+		if(queue.isEmpty())
+			return -1;
+		int e = queue.offer();
+		set.add(e);
+		return e;
+	}
+	
+	public boolean check(int number) {
+		return !set.contains(number) && number <= max;
+	}
+	
+	public void release(int number) {
+		if(set.contains(number)) {
+			set.remove(number);
+			queue.offer(number);
+		}
+	}
+}
