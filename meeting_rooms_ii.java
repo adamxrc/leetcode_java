4ceb015160a78e301e506f97f25f0d766f79df17
adamxrc <xinruichao@qq.com>
2016-11-05 20:21:51 +0800

    Update meeting_rooms_ii.java

--- meeting_rooms_ii.java
+++ meeting_rooms_ii.java
@@ -1,1 +1,22 @@
-
+public class Solution {
+    public int minMeetingRooms(Interval[] intervals) {
+        Arrays.sort(intervals, new Comparator<Interval>() {
+			public int compare(Interval i1, Interval i2) {
+				return i1.start - i2.start;
+			}
+		});
+		PriorityQueue<Interval> que = new PriorityQueue<Interval>(new Comparator<Interval>() {
+			public int compare(Interval i1, Interval i2) {
+				return i1.end - i2.end;
+			}
+		});
+		int rooms = 0;
+		for(int i = 0; i < intervals.length; i++) {
+			while(!que.isEmpty() && que.peek().end <= intervals[i].start)
+				que.poll();
+			que.offer(intervals[i]);
+			rooms = Math.max(rooms, que.size());
+		}
+		return rooms;
+    }
+}
